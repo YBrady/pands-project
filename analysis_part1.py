ddc31a2a76c84c888641f8bd3e3ca0cf7bbd8767
Yvonne Brady <yvonneabrady@gmail.com>
2019-04-10 14:40:12 +0100

    Doing a bit more

--- analysis_part1.py
+++ analysis_part1.py
@@ -12,6 +12,8 @@ import matplotlib
 
 sns.set(color_codes=True)
 
+#%matplotlib inline
+#%pylab inline
 
 
 # Declaring and setting the dataset as a global variable - saves going back and forth all the time
@@ -19,6 +21,7 @@ data = pandas.read_csv("iris_csv.csv") # All Irises
 dataVirg = data.loc[data['class'] == "Iris-virginica"] # Just the Iris-virginica
 dataVers = data.loc[data['class'] == "Iris-versicolor"]# Just the Iris-versicolor
 dataSeto = data.loc[data['class'] == "Iris-setosa"] # Just the Iris-setosa
+spices = data['class'].unique()
 
 
 # Main function
@@ -85,6 +88,7 @@ def display_raw_menu():
 		display_raw_menu()
 	elif (choice == "8"):
 		print(data.describe())
+		data.groupby('class').size()
 		display_raw_menu()
 	elif (choice == "9"):
 		display_menu()		
@@ -101,6 +105,14 @@ def display_plot_menu():
 	print("3 - Iris-Versicolor BoxPlot")
 	print("4 - All Iris BoxPlot")
 	print("5 - Compare BoxPlot")
+	print("6 - Boxplot of 4 measurements")
+	print("7 - Histogram of 4 measurements")
+	print("8 - Histogram of 4 measurements per iris class")
+	print("9 - Boxplot of 4 measurements per iris class")
+	print("10 - Violin plot - Petal Length")
+	print("11 - Scatterplot")
+	print("12 - Colour Scatterplot")
+	print("13 - Line and Scatterplot (Colour)")
 	print("0 - Return to Main Menu")
 
 	choice = input("Enter plot choice: ")
@@ -120,6 +132,25 @@ def display_plot_menu():
 			data.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 		elif (choice == "5"):
 			compare_box()
+		elif (choice == "6"):
+			#box and whisker plots for different numerical columns
+			data.plot(kind='box')
+		elif (choice == "7"):
+			#HIST PLOT OF ALL NUMERICAL COLUMNS
+			data.hist(figsize=(10,5))
+		elif (choice == "8"):
+			for spice in spices:
+				data[data['class']==spice].hist(figsize=(10,5))
+		elif (choice == "9"):
+			data.boxplot(by='class',figsize=(15,15))
+		elif (choice == "10"):
+			sns.violinplot(data=data,x='class',y='petallength')
+		elif (choice == "11"):
+			pandas.plotting.scatter_matrix(data,figsize=(15,10))
+		elif (choice == "12"):
+			sns.pairplot(data,hue="class")
+		elif (choice == "13"):
+			sns.pairplot(data,diag_kind='kde',hue='class')
 		else:
 			display_plot_menu()
 
@@ -150,7 +181,7 @@ def compare_box():
 			listOfNumericalColumns.append(column)
 
 	print('listOfNumericalColumns :',listOfNumericalColumns)
-	spices = data['class'].unique()
+#	spices = data['class'].unique()
 	print('spices :',spices)
 
 	fig, axs = plt.subplots(nrows=len(listOfNumericalColumns),ncols=len(spices),figsize=(15,15))
